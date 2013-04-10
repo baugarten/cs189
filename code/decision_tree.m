0fed2e9fcf365e4818a32cc84347981718846948
Richard Hwang <richard.hwang201@gmail.com>
2013-04-09 23:37:41 -0700

    Organizing code

--- code/decision_tree.m
+++ code/decision_tree.m
@@ -6,15 +6,22 @@
 %   - stopping_opt: stopping criteria
 %   - split_opt: splitting criteria
 function [] = decision_tree(data, stopping_opt, split_opt)
+    [x_train, y_train, x_test, y_test] = loadData(path);
+    d_tree = make_tree(x_train, y_train, stopping_opt, split_opt);
+    testing_error = test(d_tree, x_test, y_test)
+end
+
+% @params
+%   - x: training points
+%   - y: training labels
+%   - stopping_opt: stopping criteria
+%   - split_opt: splitting criteria
+% @return
+%   d_tree is a struct with two attributes, values and parents
+function d_tree = make_tree(x, y, stopping_opt, split_opt)
     % if stopping criteria not met
         % sort all by all features
         % calculate entropies (or more generally splitting criteria)
         % split on greatest reduction
         % decision_tree on both halves
-
-    % One question is how do we store the tree?  One option is as an array
-    % (hilfinger did this).  0's children are 1 and 2.  1's children are 3 and
-    % 4.  2's children are 5 and 6.  3's children are 7 and 8, and so on.
-    % Space-wise this is pretty wasteful though, as the tree probably won't
-    % be complete.  Any other ideas?
 end
---
+++ code/test.m
@@ -0,0 +1,6 @@
+% @author       Ben, Rich, Max
+% @assignment   CS189 HW5
+
+function testing_error = test(d_tree, x, y)
+    testing_error = 0;
+end
