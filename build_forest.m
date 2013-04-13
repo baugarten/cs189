ab2debad083b52d44d169904edfbc19389d4ea99
Ben Augarten <b.augarten@gmail.com>
2013-04-12 17:43:35 -0700

    Forests done, but with a surprisingly high error rate

--- build_forest.m
+++ build_forest.m
@@ -1,6 +1,8 @@
-function [forest] = build_forest( data_labels, num_trees )
+function [forest] = build_forest( data_labels, num_trees, dimensions )
     forest = {};
-    dimensions = 5;
+    if nargin < 3
+        dimensions = 20;
+    end
     for i=1:num_trees
         indices = randsample(size(data_labels, 1), size(data_labels, 1), true);
         data = data_labels(indices, :);
