4245a3a6b9e6ecda34a6d9a5b7332c4183bca6ac
Max Johnson <maxdjohnson@gmail.com>
2013-04-16 18:35:04 -0700

    added function to plot the behavior of decision trees vs stopping point

---
+++ plot_tree_entropy_n_nodes.m
@@ -0,0 +1,12 @@
+function [ output_args ] = plot_tree_entropy_n_nodes( Xtrain, ytrain, Xtest, ytest, min, max )
+%PLOT_TREE_ENTROPY_N_NODES Summary of this function goes here
+%   Detailed explanation goes here
+xspace = min:max;
+errors = zeros(size(xspace, 1), 1);
+for n=min:max,
+    node = build_node_recursive([Xtrain ytrain], 'entropy', 1, 'n_nodes', n);
+    errors(n) = evaluate_tree(node, Xtest, ytest);
+end
+plot(xspace, errors);
+end
+
