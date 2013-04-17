8615e55202b958a4d6b8d04c9991952effeb295c
Ben Augarten <b.augarten@gmail.com>
2013-04-16 17:44:07 -0700

    Working. Fixed error calculation to be probability of error, not error rate

---
+++ evaluate_tree_dist.m
@@ -0,0 +1,9 @@
+function [error_rate, predictions] = evaluate_tree(root, data, labels, dist)
+    m = size(labels, 1);
+    predictions = zeros(m, 1);
+    for j=1:m,
+        predictions(j) = tree_classify(root, data(j, :));
+    end
+
+    error_rate = sum(dist .* (predictions ~= labels));
+end
