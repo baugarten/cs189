168987ac389e6c08573a1ec04a80b39741884459
Ben Augarten <b.augarten@gmail.com>
2013-04-28 13:51:45 -0700

    Changed args to just directory

--- hw6/build_image_mat.m
+++ hw6/build_image_mat.m
@@ -1,4 +1,5 @@
-function [image_mat] = build_image_mat(dir, files)
+function [image_mat] = build_image_mat(directory)
+    files = dir(strcat(directory, '/*.jpg'));
     image_mat = zeros(size(files, 1), 51951); % 277200 = size of im_vector
     size(files, 1)
     for i=1:size(files, 1)
