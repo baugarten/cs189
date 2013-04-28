a076bc85c6e7842fb02666402b30ae2f1060b02a
Ben Augarten <b.augarten@gmail.com>
2013-04-27 17:47:48 -0700

    Added funcs to mask an image and build matrix of masked images

---
+++ hw6/build_image_mat.m
@@ -0,0 +1,7 @@
+function [image_mat] = build_image_mat(dir, files)
+    image_mat = zeros(size(files, 1), 51951); % 277200 = size of im_vector
+    size(files, 1)
+    for i=1:size(files, 1)
+        image_mat(i,:) = mask_image(strcat(dir, '/', files(i).name));
+    end
+end
--- hw6/mask_image.m
+++ hw6/mask_image.m
@@ -1,8 +1,11 @@
-function [masked_image] = mask_image(filepath)
+function [im_vector] = mask_image(filepath)
+    mask = load('mask.mat');
+    mask = mask.mask;
     unmasked_pixels = find(mask);
     image = imread(filepath);
     im_vector = image(unmasked_pixels);
-    full_im = zeros(size(mask));
-    full_im(unmasked_pixels) = im_vector;
-    masked_image = full_im;
+    
+    %full_im = zeros(size(mask));
+    %full_im(unmasked_pixels) = im_vector;
+    %masked_image = full_im;
 end
